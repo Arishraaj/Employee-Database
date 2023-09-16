b9950440dfa3c1125763d54cbbfe6d798a9118f0
Arishraaj <100360533+Arishraaj@users.noreply.github.com>
2023-09-17 00:31:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,redirect,request,flash
+from flask import Flask,render_template,url_for,redirect,request
 from flask_mysqldb import MySQL
 
 app=Flask(__name__)
@@ -6,10 +6,10 @@ app=Flask(__name__)
 app.static_folder = 'static'
 
 #sql connection
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'arish'
-app.config['MYSQL_DB'] = 'python_db'
+app.config['MYSQL_HOST'] = 'Arishraaj.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = 'Arishraaj'
+app.config['MYSQL_PASSWORD'] = 'admin@123'
+app.config['MYSQL_DB'] = 'Arishraaj$employee_data'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql=MySQL(app)
 
@@ -38,9 +38,9 @@ def edituser(id):
 			con.execute("update users set name=%s,age=%s,department=%s where id=%s",[name,age,department,id])
 			mysql.connection.commit()
 			con.close()
-			flash('Details Updated')
+
 			return redirect (url_for("home"))
-			               
+
 	con.execute("SELECT * from users where ID=%s",[id])
 	res=con.fetchone()
 	return render_template("edit.html",datas=res)
@@ -59,9 +59,9 @@ def addUser():
         con.execute("INSERT into users (NAME,AGE,DEPARTMENT) values (%s,%s,%s)",[name,age,department])
         mysql.connection.commit()
         con.close()
-        flash("New Employee Details Updated")
+
         return redirect(url_for("home"))
-    
+
     return render_template("add.html")
 
 #Search user
@@ -79,13 +79,13 @@ def search():
 
         if row:
             # The name exists, proceed with deletion
-            
+
             results=row
-            
+
         else:
             # The name doesn't exist, display a message
-            results="not_present"            
-        
+            results="Not present"
+
     return render_template("search.html", result=results)
 
 #Delete user
@@ -104,16 +104,16 @@ def delete():
             con.execute("DELETE FROM users WHERE name = %s", (name,))
             mysql.connection.commit()
             con.close()
-            flash("Employee Details Deleted")
+
             return redirect(url_for("home"))
         else:
             # The name doesn't exist, display a message
             con.close()
             return render_template("delete.html", not_present=True)
-        
+
     return render_template("delete.html", not_present=False)
 
 
 if(__name__=='__main__'):
-    app.secret_key="flash123"
+
     app.run(debug=True)
